3d21d09d681a4a5b14b80bafa4eea8ce57edd4e0
Logan Graham <LDGBK6@mail.missouri.edu>
2022-12-22 15:28:14 -0600

    this

--- GameFrame.java
+++ GameFrame.java
@@ -2,13 +2,13 @@ import javax.swing.JFrame;
 
 class GameFrame extends JFrame {
     GameFrame() {
-        add(new GamePanel());
-        setTitle("Tetromino Game");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
-        pack();
-        setLayout(null);
-        setVisible(true);
-        setLocationRelativeTo(null);
+        this.add(new GamePanel());
+        this.setTitle("Tetromino Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLayout(null);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
     }
 }
