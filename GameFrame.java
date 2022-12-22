866c3de11900e53d77dbccf585f9d15b67350802
mbowman21 <sstraberry101@gmail.com>
2022-12-22 15:09:38 -0600

    added frame

---
+++ GameFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+class GameFrame extends JFrame {
+    GameFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(null);
+        this.setVisible(true);
+    }
+}
--- TetrominoGame.java
+++ TetrominoGame.java
@@ -1,5 +1,7 @@
 public class TetrominoGame {
     public static void main(String[] args) {
         System.out.println("This is the main method.");
+        
+        new GameFrame();
     }
 }
