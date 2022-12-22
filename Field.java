58da45893a1ee5f11a9a323e740cde3e8128f56e
Logan Graham <LDGBK6@mail.missouri.edu>
2022-12-22 14:00:31 -0600

    Added field constructor

--- Field.java
+++ Field.java
@@ -1,3 +1,10 @@
 class Field {
-    
+    private Block[][] field;
+
+    private final int rowNumber = 20;
+    private final int columnNumber = 10;
+
+    Field() {
+        field = new Block[rowNumber][columnNumber];
+    }
 }
--- Game.java
+++ Game.java
@@ -1,5 +1,3 @@
-import Shapes.*;
-
 class Game {
-    LShape shape = new LShape();
+
 }
