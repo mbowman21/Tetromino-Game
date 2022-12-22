e1513550b5cea6f1231da4a8b7b788b593bdb836
Logan Graham <LDGBK6@mail.missouri.edu>
2022-12-22 15:50:13 -0600

    Added key listener

--- GamePanel.java
+++ GamePanel.java
@@ -1,15 +1,36 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JPanel;
 
 class GamePanel extends JPanel implements ActionListener {
-    GamePanel() {
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
 
+    GamePanel() {
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    break;
+            }
+        }
     }
 }
