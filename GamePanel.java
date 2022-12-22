45d74f888fbec0bd6e0bd5dcfb06a070e655c893
mbowman21 <sstraberry101@gmail.com>
2022-12-22 16:04:33 -0600

    grid lines and unit size

--- GamePanel.java
+++ GamePanel.java
@@ -8,14 +8,30 @@ import java.awt.event.KeyEvent;
 import javax.swing.JPanel;
 
 class GamePanel extends JPanel implements ActionListener {
-    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_WIDTH = 300;
     static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 30;
+
 
     GamePanel() {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.BLACK);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
+
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+        }
     }
 
     @Override
